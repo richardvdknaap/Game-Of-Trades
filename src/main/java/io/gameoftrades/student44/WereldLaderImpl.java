d9eb37783935825cc87e81a56358cfcbcc59fd19
Jack <Jacekgoog@Gmail.com>
2019-09-19 14:12:24 +0200

    Revert "Merge branch 'Jacek' of C:\Users\Gebruiker\Documents\GitHub\got19-team44 with conflicts."
    This reverts commit 19e7404752a444808b9b6716c293cb479a37f4e0.

--- src/main/java/io/gameoftrades/student44/WereldLaderImpl.java
+++ src/main/java/io/gameoftrades/student44/WereldLaderImpl.java
@@ -9,25 +9,19 @@ import io.gameoftrades.model.markt.Handelswaar;
 import io.gameoftrades.model.markt.Markt;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class WereldLaderImpl implements WereldLader {
 
     @Override
-    public Wereld laad(String resource){
-        //
-        // Gebruik this.getClass().getResourceAsStream(resource) om een resource van het classpath te lezen.
-        //
-        // Kijk in src/test/resources voor voorbeeld kaarten.
-        //
-        // TODO Laad de wereld!
-        //
-
+    public Wereld laad(String resource) {
 
         Scanner s = new Scanner(this.getClass().getResourceAsStream(resource));
 
 
-        //FIRST PART
+        //FIRST PART KAART GROOTTE
         String line = s.nextLine().trim();
         String[] data = line.split(",");
         int[] intData = new int[data.length];
@@ -38,64 +32,72 @@ public class WereldLaderImpl implements WereldLader {
         Kaart k = Kaart.metOmvang(intData[0], intData[1]);
 
 
-        //SECOND PART
+        //SECOND PART TERREIN
         line = s.nextLine();
         for (int y = 0; y < intData[1]; y++) {
             for (int x = 0; x < intData[0]; x++) {
-                char t2 = line.charAt(x);
-                TerreinType tt = TerreinType.fromLetter(t2);
-                Coordinaat c = Coordinaat.op(x, y);
-                Terrein.op(k, c, tt);
+                if(intData[0]==line.trim().length()) {
+                    char t2 = line.charAt(x);
+                    TerreinType tt = TerreinType.fromLetter(t2);
+                    Coordinaat c = Coordinaat.op(x, y);
+                    Terrein.op(k, c, tt);
+                }
+                else{
+                    throw new IllegalArgumentException();
+                }
             }
             line = s.nextLine();
         }
 
-        //THIRD PART
+        //THIRD PART STEDEN
         ArrayList<Stad> a = new ArrayList<>();
-        int cityAmount = Integer.parseInt(line);
+        int cityAmount = Integer.parseInt(line.trim());
         line = s.nextLine();
 
         for (int x = 0; x < cityAmount; x++) {
             line.trim();
             String[] citydata = line.split(",");
-            int[] cityint = new int[citydata.length];
-
             int xas = Integer.parseInt(citydata[0]);
             int yas = Integer.parseInt(citydata[1]);
-            Coordinaat coordinaat = Coordinaat.op(xas, yas);
-            String naam = citydata[2];
-            Stad stad = Stad.op(coordinaat, naam);
-            a.add(stad);
-
+            if((xas>intData[0]&&yas>intData[1])||(xas<=0&&yas<=0)){
+                throw new IllegalArgumentException();
+            }
+            else {
+                Coordinaat coordinaat = Coordinaat.op(xas, yas);
+                String naam = citydata[2];
+                Stad stad = Stad.op(coordinaat, naam);
+                a.add(stad);
+            }
 
             line = s.nextLine();
 
         }
 
-        //FOURTH PART
-        int handelAmount = Integer.parseInt(line);
-        System.out.println(handelAmount);
-        line = s.nextLine();
+        //FOURTH PART HANDEL
+        int handelAmount = Integer.parseInt(line.trim());
+        if(s.hasNextLine()) {
+            line = s.nextLine();
+        }
         ArrayList<Handel> handels = new ArrayList<>();
         for (int x = 0; x < handelAmount; x++){
             for (Stad stad : a) {
                 line.trim();
                 String[] marktData = line.split(",");
+
+                /*System.out.println(a);
+                System.out.println(marktData[0]);
+                System.out.println(!a.contains(marktData));
+                if(a.stream(marktData).anyMatch("s"::equals)){
+                    throw new IllegalArgumentException();
+                }*/
+
+
                 if(marktData[0].equals(stad.getNaam())){
-                    if(marktData[1].equals("BIEDT")){
-                        HandelType handelType = HandelType.BIEDT;
-                        Handelswaar handelswaar = new Handelswaar(marktData[2]);
-                        int prijs = Integer.parseInt(marktData[3]);
-                        Handel h = new Handel(stad,handelType,handelswaar,prijs);
-                        handels.add(h);
-                    }
-                    else{
-                        HandelType handelType = HandelType.VRAAGT;
-                        Handelswaar handelswaar = new Handelswaar(marktData[2]);
-                        int prijs = Integer.parseInt(marktData[3]);
-                        Handel h = new Handel(stad,handelType,handelswaar,prijs);
-                        handels.add(h);
-                    }
+                    HandelType handelType = HandelType.valueOf(marktData[1]);
+                    Handelswaar handelswaar = new Handelswaar(marktData[2]);
+                    int prijs = Integer.parseInt(marktData[3].trim());
+                    Handel h = new Handel(stad,handelType,handelswaar,prijs);
+                    handels.add(h);
                 }
             }
         }
