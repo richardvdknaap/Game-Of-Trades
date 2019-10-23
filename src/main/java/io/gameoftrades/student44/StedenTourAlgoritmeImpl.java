ff737b0f19d027b0ae97b1e6af8ee81f6927bfd5
Richard <richard.vd.knaap@outlook.com>
2019-10-23 22:27:42 +0200

    start genetic algorithm (3-3:14)

--- src/main/java/io/gameoftrades/student44/StedenTourAlgoritmeImpl.java
+++ src/main/java/io/gameoftrades/student44/StedenTourAlgoritmeImpl.java
@@ -5,63 +5,137 @@ import io.gameoftrades.debug.Debugger;
 import io.gameoftrades.model.Handelaar;
 import io.gameoftrades.model.algoritme.SnelstePadAlgoritme;
 import io.gameoftrades.model.algoritme.StedenTourAlgoritme;
+import io.gameoftrades.model.kaart.Coordinaat;
 import io.gameoftrades.model.kaart.Kaart;
 import io.gameoftrades.model.kaart.Pad;
 import io.gameoftrades.model.kaart.Stad;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static jdk.nashorn.internal.objects.NativeMath.random;
+
 public class StedenTourAlgoritmeImpl implements StedenTourAlgoritme, Debuggable {
+    private Debugger debugger;
     private Kaart kaart;
     private List<Stad> steden;
-    private ArrayList<Stad> stadGehad;
-    private Debugger debugger;
-    private Stad currentStad;
-    private Stad nextStad;
+    private int totalSteden;
+    private int popSize;
+    private ArrayList<Integer> order;
+    private int[] population;
+    private ArrayList<Double> fitness;
+    private int recordDistance;
+    private ArrayList<Integer> bestEver;
+    private ArrayList<Stad> bestRoute;
+    private Stad currentBest;
     private Handelaar handelaar;
     private SnelstePadAlgoritme algoritme;
-    private SnelstePadAlgoritmeImpl nieuweAlgoritme;
 
 
     @Override
     public List<Stad> bereken(Kaart _kaart, List<Stad> _list) {
         this.kaart = _kaart;
-        this.steden= _list;
-        this.stadGehad = new ArrayList<>();
-        this.currentStad = steden.get(0);
+        this.steden = _list;
+        this.totalSteden = steden.size();
+        this.popSize = 500;
+        this.currentBest = steden.get(0);
         this.handelaar = new HandelaarImpl();
         this.algoritme = handelaar.nieuwSnelstePadAlgoritme();
 
-        stadGehad.add(currentStad);
-        while(stadGehad.size() != steden.size()){
-            currentStad = getNextStad(stadGehad);
-            System.out.println(currentStad.getNaam());
+        this.order = new ArrayList<>();
+        this.bestEver = new ArrayList<>();
+        this.bestRoute = new ArrayList<>();
+        setUp();
+        for(int a:bestEver){
+            System.out.print(a + " ");
         }
-       
-        debugger.debugSteden(kaart, stadGehad);
-        return stadGehad;
+        System.out.println(recordDistance);
+
+        for(int x=0;x<200;x++) {
+            int d = calcDistance(steden, order);
+            if (d < recordDistance) {
+                recordDistance = d;
+                bestEver = order;
+            }
+            nextOrder();
+        }
+        for (int k = 0; k < bestEver.size(); k++) {
+            int n = bestEver.get(k);
+            bestRoute.add(steden.get(n));
+        }
+        System.out.println(" ");
+        for(int a:bestEver){
+            System.out.print(a + " ");
+        }
+        System.out.println(recordDistance);
+        debugger.debugSteden(kaart, bestRoute);
+        return bestRoute;
+
+
     }
 
-    public Stad getNextStad(ArrayList<Stad> stadGehad){
-        Stad dichsteStad = null;
-        double cost= -1;
-        for(Stad stad: steden){
-           if(stadGehad.contains(stad)){
-               continue;
-           }
-           Pad pad = algoritme.bereken(kaart, currentStad.getCoordinaat(), stad.getCoordinaat());
-           if(cost != -1 && pad.getTotaleTijd() >= cost){
-               continue;
-           }
-           dichsteStad = stad;
-           cost = pad.getTotaleTijd();
+    public void setUp(){
 
-
+        for(int i=0; i<steden.size();i++){
+            order.add(i);
         }
-        stadGehad.add(dichsteStad);
-        return dichsteStad;
+
+        int d = calcDistance(steden,order);
+        recordDistance = d;
+        bestEver = order;
     }
+
+    public ArrayList<Integer> swapOrder(ArrayList<Integer> a, int i, int j){
+        int temp = a.get(i);
+        a.set(i,a.get(j));
+        a.set(j,temp);
+        return a;
+    }
+
+
+    public int calcDistance(List<Stad> steden, List<Integer> order){
+        int sum = 0;
+        for (int i=0; i<order.size()-1;i++){
+            Stad stadA = steden.get(order.get(i));
+            Stad stadB = steden.get(order.get(i+1));
+            Coordinaat van = stadA.getCoordinaat();
+            Coordinaat naar = stadB.getCoordinaat();
+            int d = algoritme.bereken(kaart,van,naar).getTotaleTijd();
+            sum += d;
+        }
+        return sum;
+    }
+
+    public void nextOrder(){
+        int largestI = -1;
+        for(int i = 0; i<order.size() -1 ; i++){
+            if(order.get(i)<order.get(i+1)){
+                largestI = i;
+            }
+        }
+        if(largestI ==-1){
+            return;
+        }
+        int largestJ = -1;
+        for(int j = 0; j<order.size(); j++){
+            if(order.get(largestI)<order.get(j)){
+                largestJ = j;
+            }
+        }
+        //order = swapOrder(order,largestI,largestJ);
+        order = swapOrder(order,largestI,largestJ);
+
+        ArrayList<Integer> tail = new ArrayList<>(order.subList(largestI+1, order.size()));
+        ArrayList<Integer> head = new ArrayList<>(order.subList(0, largestI+1));
+        Collections.reverse(tail);
+        order.clear();
+        order.addAll(head);
+        order.addAll(tail);
+
+    }
+
 
     @Override
     public void setDebugger(Debugger debugger) {
